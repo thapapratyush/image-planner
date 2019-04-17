dffbaf7ec4aa68ff0a706887145fa425d6b4165b
thapapratyush <thapa.pratyush10@gmail.com>
2019-04-16 22:17:20 -0400

    Implement regex precheck for URL
    fix bug that loads image multiple times

--- image.js
+++ image.js
@@ -10,7 +10,6 @@ function addImageifValid(){
 function populateimagenode(img){
     clearErrorMsg();
     img.className = getLatestURL();
-    img.src = getLatestURL();
     document.getElementById('picture-container').appendChild(img);
 }
 
@@ -24,27 +23,33 @@ function removeImageifValid(){
     }
 }
 
+function supportsRendering(){
+    return(getLatestURL().match(/\.(jpeg|jpg|gif|png)$/) != null);
+}
+
 function validateURL(timeoutT){
-    return new Promise(function (resolve, reject) {
-        var timeout = timeoutT || 5000;
-        var timer, img = new Image();
-        img.onerror = img.onabort = function () {
-            clearTimeout(timer);
-            printErrorMsg("The image could not be loaded");
-            reject("The image could not be loaded");
-        };
-        img.onload = function () {
-            clearTimeout(timer);
-            populateimagenode(img);
-            resolve("Successfully loaded image");
+    if (supportsRendering()){
+        return new Promise(function (resolve, reject) {
+            var timeout = timeoutT || 5000;
+            var timer, img = new Image();
+            img.onerror = img.onabort = function () {
+                clearTimeout(timer);
+                printErrorMsg("The image could not be loaded");
+                reject("The image could not be loaded");
+            };
+            img.onload = function () {
+                clearTimeout(timer);
+                populateimagenode(img);
+                resolve("Successfully loaded image");
         };
         timer = setTimeout(function () {
-            img.src = "";
+            img.src = ".php";
             printErrorMsg("Image couldn't be loaded due to timeout");
             reject("Image couldn't be loaded due to timeout");
         }, timeout);
         img.src = getLatestURL();
     });
+    }
 }
 
 function printErrorMsg(errormsg){
